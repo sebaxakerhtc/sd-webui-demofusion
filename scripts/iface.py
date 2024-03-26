db03d2c6fc51a8da806c2074bc07ef4ec99990cb
sebaxakerhtc <me@sebaxakerhtc.pro>
2024-03-26 12:37:00 +0200

    Optimizations

--- scripts/iface.py
+++ scripts/iface.py
@@ -11,7 +11,7 @@ from demofusion.pipeline_demofusion_sdxl import DemoFusionSDXLPipeline
 # Webui root path
 ROOT_DIR = Path().absolute()
 
-def generate_images(prompt, negative_prompt, height, width, num_inference_steps, guidance_scale, cosine_scale_1, cosine_scale_2, cosine_scale_3, sigma, view_batch_size, stride, seed):
+def generate_images(prompt, negative_prompt, width, height, num_inference_steps, guidance_scale, cosine_scale_1, cosine_scale_2, cosine_scale_3, sigma, view_batch_size, stride, seed):
     model_ckpt = "camenduru/DemoFusion"
     pipe = DemoFusionSDXLPipeline.from_pretrained(model_ckpt, torch_dtype=torch.float16)
     pipe = pipe.to("cuda")
@@ -29,7 +29,7 @@ def generate_images(prompt, negative_prompt, height, width, num_inference_steps,
     return (images[0], images[-1])
 
 def on_ui_tabs():
-    with gr.Blocks(analytics_enabled=False) as GPT_Blocks:
+    with gr.Blocks(analytics_enabled=False) as DF_Blocks:
         gr.HTML(
             "<p id='demofusion',style=\"margin-bottom:0.75em\">DemoFusion extension for Automatic1111</p>")
         with gr.Row():
@@ -37,8 +37,8 @@ def on_ui_tabs():
                 inputs = [
                     gr.Textbox(label="Prompt"),
                     gr.Textbox(label="Negative Prompt", value="blurry, ugly, duplicate, poorly drawn, deformed, mosaic"),
-                    gr.Slider(minimum=1024, maximum=4096, step=8, value=2048, label="Height"),
                     gr.Slider(minimum=1024, maximum=4096, step=8, value=2048, label="Width"),
+                    gr.Slider(minimum=1024, maximum=4096, step=8, value=2048, label="Height"),
                     gr.Slider(minimum=10, maximum=100, step=1, value=30, label="Num Inference Steps"),
                     gr.Slider(minimum=1, maximum=20, step=0.1, value=7.5, label="Guidance Scale"),
                     gr.Slider(minimum=0, maximum=5, step=0.1, value=3, label="Cosine Scale 1"),
@@ -49,13 +49,13 @@ def on_ui_tabs():
                     gr.Slider(minimum=8, maximum=96, step=8, value=64, label="Stride"),
                     gr.Number(label="Seed", value=2013)
                 ]
-                submit_btn = gr.Button("Generate")
+                submit_btn = gr.Button(value="Generate", variant='primary')
             with gr.Column(scale=80):
                 outputs=gr.Gallery(label="Generated Images")
                 # outputs=ImageSlider(label="Comparison of SDXL and DemoFusion")
         submit_btn.click(generate_images, inputs, outputs)
 
-    return [(GPT_Blocks, "DemoFusion", "DemoFusion")]
+    return [(DF_Blocks, "DemoFusion", "DemoFusion")]
 
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
