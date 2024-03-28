c5b8a6f01744ba0465d03d8828db0ffc562f322f
sebaxakerhtc <me@sebaxakerhtc.pro>
2024-03-28 20:31:17 +0200

    NoHF

--- scripts/iface.py
+++ scripts/iface.py
@@ -6,6 +6,7 @@ import json
 import os
 import torch
 import os.path
+import random
 
 from demofusion.pipeline_demofusion_sdxl import DemoFusionSDXLStableDiffusionPipeline
 # from gradio_imageslider import ImageSlider
@@ -14,16 +15,13 @@ from demofusion.pipeline_demofusion_sdxl import DemoFusionSDXLStableDiffusionPip
 ROOT_DIR = Path().absolute()
 model_dir = "Stable-diffusion"
 vae_dir = "VAE"
+lora_dir = "Lora"
 model_path = os.path.abspath(os.path.join(paths.models_path, model_dir))
 vae_path = os.path.abspath(os.path.join(paths.models_path, vae_dir))
+lora_path = os.path.abspath(os.path.join(paths.models_path, lora_dir))
 model_list = modelloader.load_models(model_path=model_path, ext_filter=[".ckpt", ".safetensors"])
 vae_list = modelloader.load_models(model_path=vae_path, ext_filter=[".ckpt", ".safetensors"])
-sd_model_path = DemoFusionSDXLStableDiffusionPipeline.from_single_file
-sd_vae_path = AutoencoderKL.from_single_file
-hf_model_path = DemoFusionSDXLStableDiffusionPipeline.from_pretrained
-hf_vae_path = AutoencoderKL.from_pretrained
-df_model_path = sd_model_path
-df_vae_path = sd_vae_path
+lora_list = modelloader.load_models(model_path=lora_path, ext_filter=[".safetensors"])
 
 def set_checkpoint_model(selected_model):
     global model_ckpt
@@ -33,21 +31,14 @@ def set_vae_model(selected_vae):
     global model_vae
     model_vae = selected_vae
     
-def set_path_sd():
-    global df_model_path, df_vae_path
-    df_model_path = sd_model_path
-    df_vae_path = sd_vae_path
-    
-def set_path_hf():
-    global df_model_path, df_vae_path, model_ckpt, model_vae
-    df_model_path = hf_model_path
-    df_vae_path = hf_vae_path
-    model_ckpt = "camenduru/DemoFusion"
-    # model_vae = "madebyollin/sdxl-vae-fp16-fix"
+def set_lora_model(selected_lora):
+    global model_lora
+    model_lora = selected_lora
 
 def generate_images(prompt, negative_prompt, width, height, num_inference_steps, guidance_scale, cosine_scale_1, cosine_scale_2, cosine_scale_3, sigma, view_batch_size, stride, seed):
-    # vae = df_vae_path(model_vae)
-    pipe = df_model_path(model_ckpt, use_safetensors=True, torch_dtype=torch.float16)
+    vae = AutoencoderKL.from_single_file(model_vae)
+    pipe = DemoFusionSDXLStableDiffusionPipeline.from_single_file(model_ckpt, vae=vae, use_safetensors=True, torch_dtype=torch.float16, low_cpu_mem_usage=False, ignore_mismatched_sizes=True)
+    pipe.load_lora_weights(model_lora)
     pipe = pipe.to("cuda")
 
     generator = torch.Generator(device='cuda')
@@ -65,46 +56,42 @@ def generate_images(prompt, negative_prompt, width, height, num_inference_steps,
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as DF_Blocks:
         with gr.Row():
-            with gr.Tab("Stable Diffusion") as sd:
-                with gr.Row():
-                    sd_ckpt_file = gr.Dropdown(model_list, label="Model (Only SDXL Models are supported for now)", info="Stable Diffusion Model")
-                    sd_vae_file = gr.Dropdown(vae_list, label="VAE", info="Vae Model (coming soon)")
-            with gr.Tab("HuggingFace") as hf:
-                with gr.Row():
-                    hf_ckpt_file = gr.Textbox(label="Model (Only SDXL Models are supported for now)", placeholder="default:camenduru/DemoFusion", info="HuggingFace Model Path")
-                    hf_vae_file = gr.Textbox(label="VAE", placeholder="disabled because of errors", interactive=False, info="VAE Path")
+            sd_ckpt_file = gr.Dropdown(model_list, label="Model (Only SDXL Models are supported for now)", info="Stable Diffusion Model", scale=30)
+            sd_vae_file = gr.Dropdown(vae_list, label="VAE", info="Vae Model", scale=30)
+            sd_lora_file = gr.Dropdown(lora_list, label="LoRA", info="LoRA Model", scale=30)
+            set_lora_weight = gr.Slider(minimum=0, maximum=1, step=0.01, value=0.85, label="Weight", info="Lora weight", scale=10)
         with gr.Row():
             with gr.Column(scale=45):
-                inputs = [
-                    gr.Textbox(label="Prompt"),
-                    gr.Textbox(label="Negative Prompt", value="blurry, ugly, duplicate, poorly drawn, deformed, mosaic"),
-                    gr.Slider(minimum=1024, maximum=8192, step=8, value=2048, label="Width"),
-                    gr.Slider(minimum=1024, maximum=8192, step=8, value=2048, label="Height"),
-                    gr.Slider(minimum=1, maximum=100, step=1, value=30, label="Num Inference Steps"),
-                    gr.Slider(minimum=1, maximum=20, step=0.1, value=7.5, label="Guidance Scale"),
-                    gr.Slider(minimum=0, maximum=5, step=0.1, value=3, label="Cosine Scale 1"),
-                    gr.Slider(minimum=0, maximum=5, step=0.1, value=1, label="Cosine Scale 2"),
-                    gr.Slider(minimum=0, maximum=5, step=0.1, value=1, label="Cosine Scale 3"),
-                    gr.Slider(minimum=0.1, maximum=1, step=0.1, value=0.8, label="Sigma"),
-                    gr.Slider(minimum=4, maximum=32, step=4, value=16, label="View Batch Size"),
-                    gr.Slider(minimum=8, maximum=96, step=8, value=64, label="Stride"),
-                    gr.Number(label="Seed", value=2013)
-                ]
+                m_prompt = gr.Textbox(label="Prompt")
+                m_negative_prompt = gr.Textbox(label="Negative Prompt", value="blurry, ugly, duplicate, poorly drawn, deformed, mosaic")
+                m_width = gr.Slider(minimum=1024, maximum=8192, step=8, value=2048, label="Width")
+                m_height = gr.Slider(minimum=1024, maximum=8192, step=8, value=2048, label="Height")
+                m_num_inference_steps = gr.Slider(minimum=1, maximum=100, step=1, value=30, label="Num Inference Steps")
+                m_guidance_scale = gr.Slider(minimum=1, maximum=20, step=0.1, value=7.5, label="Guidance Scale")
+                m_cosine_scale_1 = gr.Slider(minimum=0, maximum=5, step=0.1, value=3, label="Cosine Scale 1")
+                m_cosine_scale_2 = gr.Slider(minimum=0, maximum=5, step=0.1, value=1, label="Cosine Scale 2")
+                m_cosine_scale_3 = gr.Slider(minimum=0, maximum=5, step=0.1, value=1, label="Cosine Scale 3")
+                m_sigma = gr.Slider(minimum=0.1, maximum=1, step=0.1, value=0.8, label="Sigma")
+                m_view_batch_size = gr.Slider(minimum=4, maximum=32, step=4, value=16, label="View Batch Size")
+                m_stride = gr.Slider(minimum=8, maximum=96, step=8, value=64, label="Stride")
+                m_seed = gr.Number(label="Seed", value=2013)
             with gr.Column(scale=80):
                 with gr.Row():
-                    submit_btn = gr.Button(value="Generate", variant="primary")
+                    submit_btn = gr.Button(value="Generate", variant="primary", scale=90)
+                    # rand_seed = gr.Checkbox(label="random seed", scale=10)
                     # cancel_btn = gr.Button(value="Cancel")
                 with gr.Row():
-                    outputs=gr.Gallery(label="Generated Images")
+                    main_outputs=gr.Gallery(label="Generated Images")
                     # outputs=ImageSlider(label="Comparison of SDXL and DemoFusion")
 
+        main_inputs = [m_prompt, m_negative_prompt, m_width, m_height, 
+        m_num_inference_steps, m_guidance_scale, m_cosine_scale_1, 
+        m_cosine_scale_2, m_cosine_scale_3, m_sigma, m_view_batch_size, m_stride, m_seed]
         sd_ckpt_file.change(set_checkpoint_model, inputs=sd_ckpt_file, outputs=sd_ckpt_file.value)
         sd_vae_file.change(set_vae_model, inputs=sd_vae_file, outputs=sd_vae_file.value)
-        hf_ckpt_file.change(set_checkpoint_model, inputs=hf_ckpt_file, outputs=None)
-        hf_vae_file.change(set_vae_model, inputs=hf_vae_file, outputs=None)
-        sd.select(set_path_sd)
-        hf.select(set_path_hf)
-        submit_btn.click(generate_images, inputs, outputs)
+        sd_lora_file.change(set_lora_model, inputs=sd_lora_file, outputs=sd_lora_file.value)
+        submit_btn.click(generate_images, inputs=main_inputs, outputs=main_outputs)
+        # DF_Blocks.load(lambda: gr.update(value=random.randrange(1, 4294967295)), None, m_seed)
         # click_event = submit_btn.click(generate_images, inputs, outputs)
         # cancel_btn.click(fn=None, inputs=None, outputs=None, cancels=[click_event])
 
